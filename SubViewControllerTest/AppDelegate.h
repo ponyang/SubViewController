207a812028e89fda0c7ff52b37d80130d3071583
Kim Iltae <nevard@letsee.io>
2015-09-11 17:34:34 +0900

    Initial Commit

---
+++ SubViewControllerTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubViewControllerTest
+//
+//  Created by nevard on 2015. 9. 11..
+//  Copyright © 2015년 letsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SubViewControllerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubViewControllerTest
+//
+//  Created by nevard on 2015. 9. 11..
+//  Copyright © 2015년 letsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
